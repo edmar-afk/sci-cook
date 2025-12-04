83c8b2f1599d5845f5bbaaf52f18bb4043eff951
edmar-afk <jaywrsnp6@gmail.com>
2025-12-04 20:15:26 +0800

    new

--- src/routes/Quiz2.jsx
+++ src/routes/Quiz2.jsx
@@ -8,7 +8,7 @@ function Quiz2() {
       <div className="mx-0 lg:mx-72 shadow-2xl pb-12">
         <div className="px-4 pt-2 lg:pt-14 ">
           <p className="lobster text-4xl text-pink-600">
-            Interactive Practice - Part 2{" "}
+            Application 
           </p>
         </div>
         <div className="mt-4 mx-2 lg:mx-14">
